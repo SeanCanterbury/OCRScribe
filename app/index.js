7eefd8a75f8f667c030e69d3b4ae8df2ce0e2100
Saamyar Alizadeh <saamyar@Saamyars-Air.attlocal.net>
2024-03-18 18:05:10 -0400

    accessing homepage

--- app/index.js
+++ app/index.js
@@ -1,38 +1,56 @@
 // app/Index.js
 import React from 'react';
-import { View, Button } from 'react-native';
-import {StyleSheet, Text} from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { View, Button, Text, StyleSheet, StatusBar } from 'react-native';
+import { useNavigation } from '@react-navigation/native'; // This line is crucial
+import Home from '../app/Home';
 
-const Index = ({ navigation }) => {
+const Stack = createStackNavigator();
+
+const HomeScreen = () => (
+  <View />
+);
+
+const Index = () => {
+  const navigation = useNavigation(); // Now useNavigation is recognized
   const handleEnterPress = () => {
     navigation.navigate('Home');
   };
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-       <Text style ={styles.Header}>OCRScribe</Text>
-          <Text>Scan handwritten documents into machine readable text</Text>
-          <StatusBar style="auto" />
-      <Button title="Enter" onPress={handleEnterPress} />
-    </View>
-  );
-};
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={styles.Header}>OCRScribe</Text>
+        <Text>Scan handwritten documents into machine readable text</Text>
+        <StatusBar style="auto" />
+        <Button title="Enter" onPress={handleEnterPress} />
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  
+    Header: {
+      fontSize: 72,
+      fontWeight: 'bold',
+      textAlign: "center",
+    },
+  });
+  
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Index" component={Index} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 
-  Header: {
-    fontSize: 72,
-    fontWeight: 'bold',
-    textAlign: "center",
-  },
-});
-
-
-export default Index;
+export default App;
