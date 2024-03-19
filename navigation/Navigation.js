fd78c755f8d997ae08d33d5494c9bf54ad553289
Josh Reddick <josh.reddick2002@gmail.com>
2024-03-19 15:56:43 -0400

    Adding Sign-out button (nonfunctional)

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -4,16 +4,40 @@ import Index from '../app/Index';
 import Home from '../app/Home';
 import Files from '../app/Files';
 import Scanning from '../app/Scanning';
+import { TouchableOpacity, Text } from 'react-native';
 
 const Stack = createStackNavigator();
 
 const Navigation = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Index" component={Index} options={{ headerShown: false }} />
-      <Stack.Screen name="Home" component={Home} options={{ title: 'Home' }} />
-      <Stack.Screen name="Files" component={Files} options={{ title: 'Files' }} />
-      <Stack.Screen name="Scanning" component={Scanning} options={{ title: 'Scanning' }} />
+      <Stack.Screen 
+        name="Index" 
+        component={Index} 
+        options={{ headerShown: false }} />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={({ navigation }) => ({
+        title: 'Home',
+        headerRight: () => (
+          <TouchableOpacity
+            style={{ marginRight: 10 }}
+              onPress={() => alert('Sign Out button pressed')} // Replace with signout functionality
+          >
+            <Text>Sign Out</Text>
+          </TouchableOpacity>
+        ),
+      })}
+    />
+      <Stack.Screen 
+        name="Files" 
+        component={Files} 
+        options={{ title: 'Files' }} />
+      <Stack.Screen 
+        name="Scanning" 
+        component={Scanning} 
+        options={{ title: 'Scanning' }} />
     </Stack.Navigator>
   );
 };
